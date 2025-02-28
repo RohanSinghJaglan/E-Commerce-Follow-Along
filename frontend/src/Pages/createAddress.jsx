ae12cd5c4e7ad76f9720c9a7d82821330c213179
Rohan Singh Jaglan <rohan.singh@kalvium.community>
2025-02-28 11:20:29 +0530

    Milestone 23 Done

--- frontend/src/Pages/createAddress.jsx
+++ frontend/src/Pages/createAddress.jsx
@@ -19,7 +19,7 @@ const CreateAddress = () => {
             address2,
             zipCode,
             addressType,
-            email: "ayan10092018@gmail.com"
+            email: "rohansj123@gmail.com",
         };
         try {
             const response = await axios.post(
